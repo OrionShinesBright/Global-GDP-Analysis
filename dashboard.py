77343ff31d80088a33707cd40ecd8825056f9adb
OrionShinesBright <rafay880333@gmail.com>
2026-02-07 00:43:48 +0500

    Add initial documentations to data_processor.py & dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,13 @@
+# SDA - Project, Spring 2026
+#
+# Taha Tahir     [24L-0677]
+# Muhammad Rafay [24L-0649]
+
+"""
+> dashboard.py
+
+> Final logical part of the program
+> Obtains the sanitized data and the results of the computations
+> Creates the graphs/charts, and prints out the results for the dashboard part
+> Handles all visualizations modularly
+"""
---
+++ data_processor.py
@@ -0,0 +1,12 @@
+# SDA - Project, Spring 2026
+#
+# Taha Tahir     [24L-0677]
+# Muhammad Rafay [24L-0649]
+
+"""
+> data_processor.py
+
+> Second logical part of the program
+> Obtains csv formatted data from the data_loader function.
+> Filters and aligns data from the given columns as required.
+"""
