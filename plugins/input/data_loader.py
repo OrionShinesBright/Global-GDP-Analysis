0a713efa481c9b0b3361c71dc3258895f1963f89
tahathetahir <tahathetahir@gmail.com>
2026-03-02 17:21:53 +0500

    Update data_loader.py
    Changes made to reflect the new formats

--- plugins/input/data_loader.py
+++ plugins/input/data_loader.py
@@ -15,11 +15,44 @@
 import json
 import csv
 import os
+from core.protocols import PipelineService
+from typing import List,Dict
 
-# Path for the user conifguration file
-CONFIG_FILE = "config.json"
 
 
+###############################################################################
+# Input Manager Class
+#
+# Takes config file as argument
+# Returns csv,json based on config file specifications
+#
+# ARG: config (dict)
+# RET: data (list) 
+
+class InputManager:
+    
+    def __init__(self,config:Dict,service:PipelineService):
+        self.config = config
+        self.service = service
+    
+    
+    #now based on config file, relevant loading function would be called:
+    def run(self):
+        input = self.config.get("input")
+        path = self.config.get("path")
+        year = self.config.get("year")
+        continent = self.config.get("continent")
+        
+        if input == "csv":
+            data = self.load_gdp_data_csv(path)
+        elif input == "json":
+            data = self.load_gdp_data_json(path)
+        else:
+            raise ValueError(f"Unknown input type in json file : {input}")
+            
+        #time to send raw data to coree-->
+        self.service.execute(data)
+        
 ###############################################################################
 # Data loader function
 #
@@ -28,23 +61,32 @@ CONFIG_FILE = "config.json"
 #
 # ARG: filepath (str)
 # RET: csv data (list)
-def load_gdp_data(filepath):
-    # Error handling
-    if not os.path.exists(filepath):
-        raise FileNotFoundError(f"CSV file not found: {filepath}")
-    # Read and return as list
-    with open(filepath, newline="", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        return list(reader)
+    def load_gdp_data_csv(self,filepath):
+        # Error handling
+        if not os.path.exists(filepath):
+            raise FileNotFoundError(f"CSV file not found: {filepath}")
+        # Read and return as list
+        with open(filepath, newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            return list(reader)
 
-
+#use of DictReader has been avoided to standardize data and not do conversions in core
+#Thus input reader, manually reads data and converts it into appropriate data types before 
+#submitting it as a key,value pair in dictionary
+    
 ###############################################################################
-# Configuration Loader Function
+# Data loader function
 #
-# Handles the file descriptors, loads contents into memory after parsing as json
+# Takes filepath as argument
+# Returns json data
 #
-# ARG:
-# RET: json stream
-def load_config():
-    with open(CONFIG_FILE) as f:
-        return json.load(f)
+# ARG: filepath (str)
+# RET: json data (list)
+    def load_gdp_data_json(self,filepath):
+        if not os.path.exists(filepath):
+            raise FileNotFoundError(f"json file not found: {filepath}")
+        # Read and return as list
+        with open(filepath, encoding="utf-8") as f:
+            reader = json.load(f)
+            return reader
+            
